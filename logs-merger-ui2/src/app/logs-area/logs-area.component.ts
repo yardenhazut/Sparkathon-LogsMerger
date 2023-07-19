e3183eea873aba5a80393d591d38d720be01b3e9
Dvir.Harazi <Dvir.harazi@nice.com>
2023-07-19 17:10:32 +0300

    add save pdf

--- logs-merger-ui2/src/app/logs-area/logs-area.component.ts
+++ logs-merger-ui2/src/app/logs-area/logs-area.component.ts
@@ -1,5 +1,10 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {DataItem} from "../model/DataItem";
+import {ApiService} from "../services/api-service";
+import { saveAs } from 'file-saver';
+import {SaveDataItem} from "../model/SaveDataItem";
+import _ from 'lodash';
+
 
 @Component({
   selector: 'logs-area',
@@ -7,31 +12,35 @@ import {DataItem} from "../model/DataItem";
   styleUrls: ['./logs-area.component.scss']
 })
 export class LogsAreaComponent {
-  private _data:DataItem[] = [];
-  private _filters:any[] = [];
-  private _excludes:any[] = [];
-  public filteredData:DataItem[] = [];
+  private _data: DataItem[] = [];
+  private _filters: any[] = [];
+  private _excludes: any[] = [];
+  public filteredData: DataItem[] = [];
 
+
+  constructor(private apiService: ApiService) {
+  }
 
   @Input()
   public get filters() {
     return this._filters;
   }
 
-  public set filters(newFilters:any[]) {
+  public set filters(newFilters: any[]) {
     this._filters = newFilters;
-    if(this._data) {
+    if (this._data) {
       this.processData();
     }
   }
+
   @Input()
   public get excludes() {
     return this._excludes;
   }
 
-  public set excludes(newExcludes:any[]) {
+  public set excludes(newExcludes: any[]) {
     this._excludes = newExcludes;
-    if(this._data) {
+    if (this._data) {
       this.processData();
     }
   }
@@ -42,39 +51,39 @@ export class LogsAreaComponent {
     return this._data;
   }
 
-  public set data(newData:DataItem[]) {
+  public set data(newData: DataItem[]) {
     this._data = newData;
     this.processData();
   }
 
   private processData() {
     this.filteredData = [];
-    if(!this._data) {
+    if (!this._data) {
       return;
     }
     const rules = this.readRules();
     const excludes = this.readExcludes();
-    const rulesRegExs = rules.map(item=>new RegExp((item).value));
-    const excludesRegExs = excludes.map(item=>new RegExp((item).value));
+    const rulesRegExs = rules.map(item => new RegExp((item).value));
+    const excludesRegExs = excludes.map(item => new RegExp((item).value));
 
-    const tempData:DataItem[] = [];
-     // includes
-    for(const element of this._data) {
+    const tempData: DataItem[] = [];
+    // includes
+    for (const element of this._data) {
       const line = element;
-      for(const element of rulesRegExs) {
+      for (const element of rulesRegExs) {
         const rule: RegExp = element;
-        if(rule.test(line.message)){
+        if (rule.test(line.message)) {
           tempData.push(line);
           break;
         }
       }
     }
-    const toExclude:DataItem[] = [];
-    for(const element of tempData) {
+    const toExclude: DataItem[] = [];
+    for (const element of tempData) {
       const line = element;
-      for(const element of excludesRegExs) {
+      for (const element of excludesRegExs) {
         const rule: RegExp = element;
-        if(rule.test(line.message)){
+        if (rule.test(line.message)) {
           toExclude.push(line);
           break;
         }
@@ -84,20 +93,43 @@ export class LogsAreaComponent {
     this.filteredData = result;
   }
 
-  private read(key:string){
+  private read(key: string) {
     const ret = localStorage.getItem(key);
-    let retArr:any[] = [];
-    if(ret) {
+    let retArr: any[] = [];
+    if (ret) {
       retArr = JSON.parse(ret);
     }
-    return retArr.filter(item=>item.selected);
+    return retArr.filter(item => item.selected);
   }
-  private readRules():any[] {
+
+  private readRules(): any[] {
     return this.read("FilterLabels");
   }
 
-  private readExcludes():any[] {
+  private readExcludes(): any[] {
     return this.read("ExcludeLabels");
   }
 
+  onSave() {
+    const copiedData = _.cloneDeep(this.filteredData);
+    copiedData.forEach(item=>item.message = item.message.replaceAll("<br>", "\n\n"));
+
+    const saveDataItem:SaveDataItem = {
+      "logRows": copiedData,
+      "desiredFormat": "PDF",
+      "query": "aaa"
+    };
+    this.apiService.postSave(saveDataItem).subscribe(
+      response => {
+
+        console.log("lal")
+        // Convert the Blob to a MIME type
+        const fileBlob = new Blob([response], { type: 'application/pdf' });
+        saveAs(fileBlob, 'downloaded_file.pdf');
+      },
+      error => {
+        console.error('Error:', error);
+      }
+    );
+  }
 }
