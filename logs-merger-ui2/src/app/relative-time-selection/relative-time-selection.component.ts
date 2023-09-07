771dea80ee56611096208408970c215215172dec
Maayan Hope <maayan.hope@nice.com>
2023-09-07 12:51:00 +0300

    search by minutes

--- logs-merger-ui2/src/app/relative-time-selection/relative-time-selection.component.ts
+++ logs-merger-ui2/src/app/relative-time-selection/relative-time-selection.component.ts
@@ -7,6 +7,8 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class RelativeTimeSelectionComponent {
 
+  timeType:number = 0;
+
   @Output()
   relativeTermChange:EventEmitter<number> = new EventEmitter<number>();
 
